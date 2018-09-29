662460630a8acc45432b25f6c3b5724b6e12d3f4
ari1411.lobos <ari1411.lobos@gmail.com>
2018-09-28 21:18:02 -0300

    clase

---
+++ Dominio/Domicilio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Domicilio
+    {
+        public int IdDomicilio { set; get; }
+        public string Calle { set; get; }
+        public int Altura { set; get; }
+        public string Piso { set; get; }
+        public string Dpto { set; get; }
+        public string Municipio { set; get; }
+        public string CP { set; get; }
+
+    }
+}
